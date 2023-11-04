d5d63195699aa04c840257083d662b778de26ad2
Zach Nett <zjnett@mac.com>
2023-11-04 11:20:33 -0700

    Add C# specific stack handling

--- addons/tracer/tracer.gd
+++ addons/tracer/tracer.gd
@@ -20,18 +20,26 @@ class Trace:
 		Time.get_datetime_string_from_system()
 	)
 	var thread_id := 1
+	var _has_mono: bool = Engine.has_singleton("GodotSharp")
 
 	func _init(
 		msg: String, level: Level, new_thread_id := 0
 	) -> void:
 		self.msg = msg
 		self.level = level
-		var st = get_stack()
-		if st.size() > 2:
-			module = st[2].source.trim_prefix("res://")
-			function_name = st[2].function
+		if _has_mono:
+			# C#-specific stack handling
+			var mono_sh_script = load("res://addons/tracer/StackHandler.cs")
+			var mono_stack_handler = mono_sh_script.new()
+			module = mono_stack_handler.GetModulePath()
+			function_name = mono_stack_handler.GetFunctionName()
 		else:
-			function_name = "unknown"
+			var st = get_stack()
+			if st.size() > 2:
+				module = st[2].source.trim_prefix("res://")
+				function_name = st[2].function
+			else:
+				function_name = "unknown"
 		thread_id = new_thread_id
 
 
